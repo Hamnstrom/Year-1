1c7c13a5784384607cefb6a588b9034ba593d1ac
Hamnstrom <150655352+Hamnstrom@users.noreply.github.com>
2024-01-22 12:27:36 +0200

    Done with Ovn-1-xtra

--- JSOvnDel1/ovn-1-xtra/script.js
+++ JSOvnDel1/ovn-1-xtra/script.js
@@ -6,4 +6,36 @@ var text="Kalle tycker om att koda"
 console.log(text.length)
 
 // 3 
-var text3=text
+var text3="Kalle tycker mycket om att koda"
+var text3length=text3.length-3
+console.log(text3length)
+
+// 4
+var text4="Kalle tycker mycket om at koda"
+var text4Out=text3.split(" ")
+console.log(text4Out[2])
+
+// 5
+var text5="Kalle tappade intresset"
+var text5Out=text5.replace("tappade","inte")
+console.log(text5Out)
+
+// 6
+console.log(text5Out.toUpperCase())
+
+// 7
+var var7=9
+
+// 8
+var var7Out=Math.pow(var7, 2) // or var var7Out=var7 ** 2
+console.log(var7Out)
+
+// 9
+console.log(var7Out.toFixed(2).replace(",", "."))
+
+
+// 10
+var var10="9"
+console.log(parseInt(var10))
+console.log(typeof(parseInt(var10)))
+
