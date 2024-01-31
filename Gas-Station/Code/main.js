28ba9859262061415a9661d94178174d97a84e13
Hamnstrom <150655352+Hamnstrom@users.noreply.github.com>
2024-01-31 13:57:13 +0200

    Css done JS to go
    Counter missing
    buttons don't do nothing

---
+++ Gas-Station/Code/main.js
@@ -0,0 +1,12 @@
+function press(fuel){
+    switch(fuel){
+        case "95": let nineFive=true;
+        case "98": let nineEight=true;
+        case "diesel": let diesel=true;
+        case "gas": let gas=true;
+        case "E10": let E10=true;
+        case "CNG": let CNG=true;
+    }
+}
+
+
