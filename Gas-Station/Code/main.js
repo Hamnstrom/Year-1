ff4ebac7b7db364b2412fcf13a48bd63d8231953
Hamnstrom <150655352+Hamnstrom@users.noreply.github.com>
2024-02-03 23:22:37 +0000

    Added features
    added:
    1. Styling
    2. Prices

--- Gas-Station/Code/main.js
+++ Gas-Station/Code/main.js
@@ -1,12 +1,35 @@
-function press(fuel){
-    switch(fuel){
-        case "95": let nineFive=true;
-        case "98": let nineEight=true;
-        case "diesel": let diesel=true;
-        case "gas": let gas=true;
-        case "E10": let E10=true;
-        case "CNG": let CNG=true;
-    }
+
+/*
+let nineFive=1.829;
+let nineEight=1.984;
+let diesel=1.995;
+let e10=1.964;
+let cng=2.490;
+*/
+
+let prices=[
+    {price:1.829,fuel:"nineFive"},
+    {price:1.984,fuel:"nineEight"},
+    {price:1.995,fuel:"diesel"},
+    {price:1.964,fuel:"e10"},
+    {price:2.490,fuel:"cng"}
+]
+
+let indexx=0;
+for(let i=0;i<prices.length;i++){
+    indexx++;
+
+    document.getElementById(indexx).innerHTML+=prices[indexx-1].price;
+
 }
 
 
+// placeholder
+sum=55115;
+console.log(`sum=${sum}`);
+
+// Reset Sum
+function setSum(value){
+    sum=value;
+    console.log(`sum=${sum}`);
+}
