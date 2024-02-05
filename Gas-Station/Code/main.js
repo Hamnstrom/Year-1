87724cd49193511f24470762402891cf54d041e0
Hamnstrom <150655352+Hamnstrom@users.noreply.github.com>
2024-02-05 13:58:24 +0200

    Not done yet
    Todo:
    1. Payment system
    2. make pump

--- Gas-Station/Code/main.js
+++ Gas-Station/Code/main.js
@@ -1,35 +1,27 @@
 
-/*
-let nineFive=1.829;
-let nineEight=1.984;
-let diesel=1.995;
-let e10=1.964;
-let cng=2.490;
-*/
-
 let prices=[
-    {price:1.829,fuel:"nineFive"},
-    {price:1.984,fuel:"nineEight"},
-    {price:1.995,fuel:"diesel"},
-    {price:1.964,fuel:"e10"},
-    {price:2.490,fuel:"cng"}
+    {price:1.123,fuel:"95"},
+    {price:2.345,fuel:"98"},
+    {price:3.456,fuel:"Diesel"},
+    {price:4.567,fuel:"E10"},
+    {price:5.678,fuel:"CNG"}
 ]
 
-let indexx=0;
-for(let i=0;i<prices.length;i++){
-    indexx++;
-
-    document.getElementById(indexx).innerHTML+=prices[indexx-1].price;
-
+for(let i=0;i<5;i++){
+    document.getElementById(i+1).innerHTML=prices[i].fuel+"="+prices[i].price+"/liter";
 }
 
 
-// placeholder
-sum=55115;
-console.log(`sum=${sum}`);
 
-// Reset Sum
-function setSum(value){
-    sum=value;
-    console.log(`sum=${sum}`);
+function foobar(option){
+    let volume;
+
+    switch(option){
+        case 1:volume=10/fuelPrice;
+        case 2:volume=25/fuelPrice;
+        case 3:volume=50/fuelPrice;
+        case 4:volume=100/fuelprice;
+    }
 }
+
+
