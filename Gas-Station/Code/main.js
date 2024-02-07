e17c2951a76cb0861316ac88237a52fe89a45d55
Hamnstrom <150655352+Hamnstrom@users.noreply.github.com>
2024-02-07 13:05:48 +0200

    Pump has been implemented
    todo:
    1. set limit by price not pumped fuel
    2. implement paying system (with: bonus, cash, pincode)
    3. cleaner UI

--- Gas-Station/Code/main.js
+++ Gas-Station/Code/main.js
@@ -7,21 +7,46 @@ let prices=[
     {price:5.678,fuel:"CNG"}
 ]
 
+
+
+//display prices in id's: 1-5
 for(let i=0;i<5;i++){
     document.getElementById(i+1).innerHTML=prices[i].fuel+"="+prices[i].price+"/liter";
 }
 
+let mouseDown=0;
+        
+let pumped=0;
+let price=2.123;
+let limit;
+limit=limit/price;
+let finalPrice=pumped*price;
 
+function pump(){
 
-function foobar(option){
-    let volume;
-
-    switch(option){
-        case 1:volume=10/fuelPrice;
-        case 2:volume=25/fuelPrice;
-        case 3:volume=50/fuelPrice;
-        case 4:volume=100/fuelprice;
+    if(pumped<limit){
+        pumped+=0.027284584;
     }
+    else if(pumped>limit){
+        pumped=limit
+    }
+    else{
+        console.log("error01")
+    }
+
+    console.log(pumped)
 }
 
+function pumpCheck(){
+    if(mouseDown==1){
+        pump()
+    }
+    else{
+        null
+    }
+    let finalPrice=pumped*price;
+    document.getElementById("out").innerHTML=`Pumped: ${pumped.toFixed(3)}L`
+    document.getElementById("outprice").innerHTML=`Price: ${finalPrice.toFixed(2)}€`
+}
 
+setInterval(pumpCheck, 2)
